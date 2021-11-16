82290af9d3cdfd1b8c9380fd673a218cf395f331
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2021-11-16 13:05:33 +0000

    Refactored tests and code to pass all tests

--- FizzBuzz/spec/fizzBuzzSpec.js
+++ FizzBuzz/spec/fizzBuzzSpec.js
@@ -1,12 +1,31 @@
 describe("Fizzbuzz", function() {
   let fizzbuzz;
-  it("should be divisible by 3 ", function() {
+
+  beforeEach(function () {
     fizzbuzz = new Fizzbuzz();
-    expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+  });
+
+  it("should be divisible by 3 ", function() {
+    expect(fizzbuzz.play(12)).toEqual('Fizz');
+  });
+
+  it("should not be divisible by 3 ", function() {
+    expect(fizzbuzz.play(16)).toEqual(16);
   });
 
   it("should be divisible by 5 ", function() {
-    fizzbuzz = new Fizzbuzz();
-    expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    expect(fizzbuzz.play(10)).toEqual('Buzz');
+  });
+
+  it("should not be divisible by 5 ", function() {
+    expect(fizzbuzz.play(8)).toEqual(8);
+  });
+
+  it("should be divisible by 15 ", function() {
+    expect(fizzbuzz.play(30)).toEqual('FizzBuzz');
+  });
+
+  it("should not be divisible by 3 or 5  ", function() {
+    expect(fizzbuzz.play(22)).toEqual(22);
   });
 });
--- FizzBuzz/src/fizzBuzz.js
+++ FizzBuzz/src/fizzBuzz.js
@@ -1,24 +1,21 @@
 class Fizzbuzz {
   
-  
-  isDivisibleByThree(number){
-    if (number % 3 == 0 ){
-      return true;
-    }
-    else {
-      return false;
-    }
-
+  _isDivisibleBy(divisor, number) {
+    return number % divisor === 0;
   }
 
-  isDivisibleByFive(number){
-    if (number % 5 == 0 ){
-      return true;
+  play(number) {
+    if (this._isDivisibleBy(15, number)) {
+      return 'FizzBuzz';
+      }
+    if (this._isDivisibleBy(3, number)) {
+      return 'Fizz';
+      }
+    if (this._isDivisibleBy(5, number)) {
+      return 'Buzz';
     }
     else {
-      return false;
-    }
-
+      return number;
+   }
   }
-
 }
